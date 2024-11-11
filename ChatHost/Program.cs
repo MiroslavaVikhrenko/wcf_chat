758d4663e293720187772d87f243468d540c1cf1
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-11-11 16:39:37 -0700

    Implement Host

--- ChatHost/Program.cs
+++ ChatHost/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace ChatHost
 {
     internal class Program
     {
+        //Host implementation
+        //need to connect System.ServiceModel
         static void Main(string[] args)
         {
+            using (var host = new ServiceHost(typeof(wcf_chat.ServiceChat)))
+            {
+                //open host
+                host.Open();
+                Console.WriteLine("Host started.");
+                //as it's a console app we call Console.ReadLine() method to prevent console from closing
+                Console.ReadLine();
+            }
         }
     }
 }
