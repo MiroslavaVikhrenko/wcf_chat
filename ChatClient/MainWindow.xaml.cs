de7d45c4050ce46c2106566b4c02cfaf1b030595
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-11-12 17:41:01 -0700

    Add load main window functionality and connect/disconnect functionality

--- ChatClient/MainWindow.xaml.cs
+++ ChatClient/MainWindow.xaml.cs
@@ -12,17 +12,29 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChatClient.ServiceChat;
 
 namespace ChatClient
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    //In order IServiceChatCallback can pass messages - in the clien we need to implement this interface
+    public partial class MainWindow : Window, IServiceChatCallback
     {
         //implemetation for the client side (graphical interface)
         //bool varuable for understanding if the user is currently connected to the service/server or not; default value is false
         bool IsConnected = false;
+
+        //create an object of host type in client so that we can interact with its methods
+        //as we already connected it in references (connected services) => we can already use it here
+        ServiceChatClient client;
+        int ID;
+
+        //Initialization will take place when calling event Window_Loaded() for our main window
+        //When main window is loaded = when the client is loaded => we will create and provide memory for service chat client object
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //when window is being created, a service chat client object is created and we will be able to interact with this object
+            client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -34,6 +46,9 @@ namespace ChatClient
             //check variable IsConnected - if the user is not yet connected - then change value for this variable to true
             if (!IsConnected)
             {
+                //pass name of the user from text box UserName - Text property
+                //this method returns ID
+                ID = client.Connect(tbUserName.Text);
                 //when the user is connected we need to block functionality to change user name => we set property IsEnabled to false
                 tbUserName.IsEnabled = false;
                 //change the button text from Connect to Disconnect so that we can re-use the button for both connect and disconnect
@@ -48,6 +63,7 @@ namespace ChatClient
         {
             if (IsConnected)
             {
+                client.Disconnect(ID);
                 tbUserName.IsEnabled = true; //when the user is disconnected, we can change user name
                 bConnDicon.Content = "Connect";
                 IsConnected = false;
@@ -71,5 +87,14 @@ namespace ChatClient
                 ConnectUser();
             }
         }
+
+        //Implement IServiceChatCallback interface
+        public void MsgCallback(string msg)
+        {
+            //add new item (=message) to the listbox where we store messages
+            //each time server will send us a message => this message will appear in the list box
+            lbChat.Items.Add(msg);
+        }
+
     }
 }
